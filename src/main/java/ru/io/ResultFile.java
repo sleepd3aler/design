be261768aa9f18bf95d43f45933d56da8bec47b1
sleepd3aler <enjoydeathcore228@gmail.com>
2025-09-15 23:11:45 +0300

    33.3 FileOutputStream

--- src/main/java/ru/io/ResultFile.java
+++ src/main/java/ru/io/ResultFile.java
@@ -3,27 +3,21 @@ package ru.io;
 import java.io.FileOutputStream;
 
 public class ResultFile {
-    public static int[][] multiply() {
-        int[][] result = new int[10][10];
-        for (int i = 0; i < result.length; i++) {
-            for (int j = 0; j < result.length; j++) {
-                result[i][j] = (i + 1) * (j + 1);
+    public static StringBuilder multiplyTable(int size) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                int number = i * j;
+                result.append(String.format("%4d", number));
             }
+            result.append(System.lineSeparator());
         }
         return result;
     }
 
     public static void main(String[] args) {
-        int[][] multiplicationTable = multiply();
-        StringBuilder outputBuilder = new StringBuilder();
-        for (int[] matrix : multiplicationTable) {
-            for (int j = 0; j < multiplicationTable.length; j++) {
-                outputBuilder.append(String.format("%4d", matrix[j]));
-            }
-            outputBuilder.append(System.lineSeparator());
-        }
         try (FileOutputStream output = new FileOutputStream("data/dataresult.txt")) {
-            output.write(outputBuilder.toString().getBytes());
+            output.write(multiplyTable(10).toString().getBytes());
         } catch (Exception e) {
             e.printStackTrace();
         }
