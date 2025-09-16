54bee2cfa5212589586e5197edc996e2cd87a6f3
sleepd3aler <enjoydeathcore228@gmail.com>
2025-09-16 16:05:51 +0300

    33.4 FileInputStream

--- src/main/java/ru/io/EvenNumberFile.java
+++ src/main/java/ru/io/EvenNumberFile.java
@@ -1,23 +1,26 @@
 package ru.io;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 
 public class EvenNumberFile {
+    public static void evenOutputBuilder(FileInputStream inputStream) throws IOException {
+        StringBuilder text = new StringBuilder();
+        int read;
+        while ((read = inputStream.read()) != -1) {
+            text.append((char) read);
+        }
+        String[] lines = text.toString().split(System.lineSeparator());
+        for (String line : lines) {
+            if (Integer.parseInt(line) % 2 == 0) {
+                System.out.println("The number: " + line + " is even");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         try (FileInputStream input = new FileInputStream("data/even.txt")) {
-            StringBuilder text = new StringBuilder();
-            int read;
-            while ((read = input.read()) != -1) {
-                    text.append((char) read);
-            }
-            String[] lines = text.toString().split(System.lineSeparator());
-            for (String line : lines) {
-                if (Integer.parseInt(line) % 2 != 0) {
-                    continue;
-                }
-                System.out.println(line);
-
-            }
+            evenOutputBuilder(input);
         } catch (Exception e) {
             e.printStackTrace();
         }
