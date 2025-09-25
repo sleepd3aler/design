db619333fd2d695cadfb259896a42c8c67288442
sleepd3aler <enjoydeathcore228@gmail.com>
2025-09-25 14:45:24 +0300

    33.18 Валидация параметров

--- src/main/java/ru/io/Search.java
+++ src/main/java/ru/io/Search.java
@@ -1,16 +1,10 @@
 package ru.io;
 
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
-
-import static java.nio.file.FileVisitResult.CONTINUE;
 
 public class Search {
     private Path root;
@@ -27,44 +21,6 @@ public class Search {
 
     public String getExtension() {
         return extension;
-    }
-
-    class SearchFiles implements FileVisitor<Path> {
-
-        private final Predicate<Path> condition;
-
-        private final List<Path> paths = new ArrayList<>();
-
-        public SearchFiles(Predicate<Path> condition) {
-            this.condition = condition;
-        }
-
-        public List<Path> getPaths() {
-            return paths;
-        }
-
-        @Override
-        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-            return CONTINUE;
-        }
-
-        @Override
-        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-            if (condition.test(file)) {
-                this.paths.add(file);
-            }
-            return CONTINUE;
-        }
-
-        @Override
-        public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-            return CONTINUE;
-        }
-
-        @Override
-        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-            return CONTINUE;
-        }
     }
 
     public List<Path> search(Path root, Predicate<Path> condition) throws IOException {
