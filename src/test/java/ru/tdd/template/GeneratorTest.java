b2936f30c44513b43b70315e576c1adbfe3063aa
sleepd3aler <enjoydeathcore228@gmail.com>
2026-03-28 17:42:41 +0300

    48.4 Шаблонизатор

--- src/test/java/ru/tdd/template/GeneratorTest.java
+++ src/test/java/ru/tdd/template/GeneratorTest.java
@@ -18,6 +18,14 @@ class GeneratorTest {
     }
 
     @Test
+    void whenMapContainsMoreKeysThanTemplateThenExceptionThrown() {
+        String temp = "I am ${name}, My age is ${age}, My phone is: ! ";
+        Map<String, String> map = Map.of("name", "Alex", "age", "32", "phone", "89788133712");
+        assertThatThrownBy(() -> generator.produce(temp, map))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
     void whenTemplateContainsIllegalKeysAmountThenExceptionThrown() {
         String temp = "I am ${name}, My age is ${age}, ${I will make ur app crash}! ";
         Map<String, String> map = Map.of("name", "Alex", "age", "32");
@@ -51,9 +59,15 @@ class GeneratorTest {
 
     @Test
     void whenTemplateIsCorrectThatExceptedResult() {
-        String temp = "I am ${name}, My age is ${age}! ";
-        Map<String, String> map = Map.of("name", "Alex", "age", "32");
-        String expected = "I am Alex, My age is 32!";
+        String temp = "I am ${name}, My age is ${age}! I have a ${animal} with name: ${nickname}, his favorite food is: ${food}.";
+        Map<String, String> map = Map.of(
+                "name", "Alex",
+                "age", "32",
+                "animal", "cat",
+                "nickname", "Soma",
+                "food", "shrimp"
+        );
+        String expected = "I am Alex, My age is 32! I have a cat with name: Soma, his favorite food is: shrimp.";
         String res = generator.produce(temp, map);
         assertThat(res).isEqualTo(expected);
     }
@@ -70,6 +84,18 @@ class GeneratorTest {
     void whenMapContainsSameValueAsTheKeyThenExceptionThrown() {
         String temp = "I am ${name}, My age is ${age}! ";
         Map<String, String> map = Map.of("name", "name", "age", "age");
+        assertThatThrownBy(() -> generator.produce(temp, map))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void whenMapContainsIllegalSymbolsThenExceptionThrown() {
+        String temp = "This ID: ${id}, name: ${name}, age: ${age}, phone ${phone} doesn`t contains illegal symbols ";
+        Map<String, String> map = Map.of(
+                "id", "${15}",
+                "name", "$name",
+                "age", "{age",
+                "phone", "123}");
         assertThatThrownBy(() -> generator.produce(temp, map))
                 .isInstanceOf(IllegalArgumentException.class);
     }
