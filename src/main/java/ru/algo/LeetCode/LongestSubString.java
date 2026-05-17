6321e5a836ec12c247608a237ba0fdcfe7baecda
sleepd3aler <enjoydeathcore228@gmail.com>
2026-05-17 23:03:52 +0300

    50.2 Алгоритм двух указателей(LongestSubstring)

--- src/main/java/ru/algo/LeetCode/LongestSubString.java
+++ src/main/java/ru/algo/LeetCode/LongestSubString.java
@@ -7,35 +7,37 @@ package ru.algo.LeetCode;
 //Output: 3
 //Explanation: The answer is "abc", with the length of 3. Note that "bca" and "cab" are also correct answers.
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LongestSubString {
     public static int lengthOfLongestSubstring(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        List<Integer> lengths = new ArrayList<>();
+        Set<Character> chars = new HashSet<>();
         char[] string = s.toCharArray();
-        for (int i = 0; i < s.length(); i++) {
-            map.put(string[i], i);
-        }
-        if (map.size() == 1) {
-            return 1;
-        }
-        int counter = 0;
-        for (int i = 0; i < string.length; i++) {
-            char current = string[i];
-            if (map.get(current) == i) {
-                lengths.add(counter);
-                counter = 0;
+        int start = 0;
+        int end = 1;
+        int max = 0;
+        while (start < string.length) {
+            char current = string[start];
+            chars.add(current);
+            while (end < string.length) {
+                char next = string[end];
+                if (chars.contains(next)) {
+                    max = Math.max(max, end - start);
+                    break;
+                } else {
+                    chars.add(next);
+                }
+                end++;
             }
-            counter++;
+            chars.clear();
+            start++;
+            end = start + 1;
         }
-        return lengths.stream().max(Integer::compareTo).orElse(0);
+        return max;
     }
 
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("bbbbb"));
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
     }
 }
