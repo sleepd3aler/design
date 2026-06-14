d1755ef4321854b01c535c660a0ff41ebb31dd26
sleepd3aler <enjoydeathcore228@gmail.com>
2026-06-14 20:51:06 +0300

    50.9 Жадный алгоритм

---
+++ src/main/java/ru/algo/greedy/GreedyAlgorithm.java
@@ -0,0 +1,24 @@
+package ru.algo.greedy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GreedyAlgorithm {
+    public static List<Integer> getChange(int amount) {
+        int[] coins = {50, 25, 10, 5, 1};
+        List<Integer> result = new ArrayList<>();
+        for (int coin : coins) {
+            while (amount >= coin) {
+                amount -= coin;
+                result.add(coin);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int amount = 93;
+        List<Integer> change = getChange(amount);
+        System.out.println("Сдача для " + amount + " : " + change);
+    }
+}
